ec78bb40962f49af04a6bcd6b483a9326d5fc24a
40323250 <b850617@yahoo.com.tw>
2015-10-23 19:20:17 +0800

    blog list

--- user/40323250/pelican/publishconf.py
+++ user/40323250/pelican/publishconf.py
@@ -25,7 +25,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
+LINKS = (('Version_1 ', 'http://b850617.github.io/2015cpHW/'),('Version_2 ', 'http://b850617.github.io/reveal2/'),('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
 
 # Social widget
