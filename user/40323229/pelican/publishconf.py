1d83a3c9c03897f857206340520201a5859e9a3a
tsrnnash <40323229@gm.nfu.edu.tw>
2015-11-09 02:41:09 +0800

    40323229

--- user/40323229/pelican/publishconf.py
+++ user/40323229/pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = '40323229'
-SITENAME = '2015FALL 40323229 CADPB 作業'
+SITENAME = '2015FALL 4023229 CADPB 作業'
 SITEURL = ''
 
 PATH = 'content'
