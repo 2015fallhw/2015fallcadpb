694813e38f4ace2365f613ff22c0655bdb739666
40323218 <judy850706@gmail.com>
2015-11-09 02:58:17 +0800

    40323229

--- user/40323229/pelican/publishconf.py
+++ user/40323229/pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '9999'
-SITENAME = '2015FALL 9999 CADPB 作業'
+AUTHOR = '40323229'
+SITENAME = '2015FALL 40323229 CADPB 作業'
 SITEURL = ''
 
 PATH = 'content'
