8475733d70cc76ce4c71b3c8412d3d3c06867dfe
40323218 <judy850706@gmail.com>
2015-11-17 23:04:30 +0800

    40323218

--- user/40323218/pelican/publishconf.py
+++ user/40323218/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('個人倉儲', 'http://40323218.github.io/40323218cphw/user/40323218/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('個人倉儲網站', 'http://40323218.github.io/40323218cphw/user/40323218/'),('個人github倉儲網站', 'https://github.com/40323218/40323218cphw/tree/gh-pages'),('2015作業主頁', '../../'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
