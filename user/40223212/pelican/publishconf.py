5e43e4380c930601a0d58aef9b2fa404e7e9a294
40323108 <40323108@gm.nfu.edu.tw>
2015-11-25 17:40:00 +0800

    creat the the file 40223212.leo

--- user/40223212/pelican/publishconf.py
+++ user/40223212/pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '9999'
-SITENAME = '2015FALL 9999 CADPB 作業'
+AUTHOR = '40223212'
+SITENAME = '2015FALL 40223212 CADPB 作業'
 SITEURL = ''
 
 PATH = 'content'
