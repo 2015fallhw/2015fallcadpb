aaf7ea9a0603be6304e049b5ad1f9befaf40cc61
40323253@gm.nfu.edu.tw <40323223@gm.nfu.edu.tw>
2015-12-05 19:09:18 +0800

    40323253

--- user/40323253/pelican/publishconf.py
+++ user/40323253/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('Version_1', 'http://b850617.github.io/2015cpHW/'),('Version_2', 'http://b850617.github.io/reveal2//'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('My github', 'https://github.com/40323253/cadp/tree/gh-pages'),('My HW pages', 'http://40323253.github.io/cadp/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
