f1ede734f020d6f282efa9d402abc1ed0207ff84
40323233 <40323233@gm.nfu.edu.tw>
2015-12-29 18:28:05 +0800

    50

--- user/40023256/pelican/publishconf.py
+++ user/40023256/pelican/publishconf.py
@@ -25,7 +25,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
+LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),('個人作業倉儲', 'http://40023256.github.io/40023256cadp/'),
          ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
 
 # Social widget
