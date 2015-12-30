cd7a0f48fede6db2fe578a8ad438c1710c755362
nashnash <40323213@gm.nfu.edu.tw>
2015-12-30 16:12:44 +0800

    151230 40323213

--- user/40323213/pelican/publishconf.py
+++ user/40323213/pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '40323213'
-SITENAME = '2015FALL 40323213 CADPB 作業'
+AUTHOR = 'KMOL'
+SITENAME = '2015FALL KMOL 課程'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,15 +26,15 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('個人倉儲網站', 'http://nashnash.github.io/40323213cphw/'),('個人github倉儲網站', 'https://github.com/nashnash/40323213cphw/tree/gh-pages'),('Group9倉儲網站', 'http://2015fallhw.github.io/2015fallcadpb/category/g9.html'))
 
 # Social widget
-SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
-          ('github', 'http://github.com/ametaireau'),)
-          
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
 DEFAULT_PAGINATION = 10
 
-SITEURL = 'http://coursemdetw.github.io/reveal'
+SITEURL = 'http://example.com'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -45,13 +45,5 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-DISQUS_SITENAME = "2015fall"
+DISQUS_SITENAME = "2015Fall"
 #GOOGLE_ANALYTICS = ""
-
-# 必須絕對目錄或相對於設定檔案所在目錄
-PLUGIN_PATHS = ['./../../../plugin']
-PLUGINS = ['liquid_tags.notebook']
-# 目錄設定相對於 reveal 下的 content 目錄
-NOTEBOOK_DIR = 'notebook'
-
-
