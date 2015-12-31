ecdd03e91ad92736914cee60802ca13757d56462
40323255 <40323255@gm.nfu.edu.tw>
2015-12-31 16:26:15 +0800

    40323255

--- user/40323255/pelican/publishconf.py
+++ user/40323255/pelican/publishconf.py
@@ -25,7 +25,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
+LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),('個人作業倉儲','http://40323255.github.io/40323255cadp/'),
          ('Python', 'http://python.org/'),('Version_1', 'http://b850617.github.io/2015cpHW/'),('Version_2', 'http://b850617.github.io/reveal2//'),('2015作業主頁', '../../'))
 
 # Social widget
