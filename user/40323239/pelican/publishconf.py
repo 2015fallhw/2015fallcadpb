4403f45c0d2cc49d436629040095903a29bad94d
40323239 <40323239@gm.nfu.edu.tw>
2016-01-07 01:22:01 +0800

    40323239

--- user/40323239/pelican/publishconf.py
+++ user/40323239/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../')('班級共同作業','http://2015fallhw.github.io/2015fallcadpb/category/g9.html'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('班級共同作業','http://2015fallhw.github.io/2015fallcadpb/category/g9.html'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
