29c6aaac45711ee2402dff00804e80b85e079f07
40323248 <40323248@gm.nfu.edu.tw>
2016-01-07 11:40:06 +0800

    40323214

--- user/40323223/pelican/publishconf.py
+++ user/40323223/pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = '40323223'
-SITENAME = '2015FALL 40323223 CADPB 作業'
+SITENAME = '2015FALL 40323223 電腦輔助設計實習 作業'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('My Github', 'https://github.com/asd12357631/ideal/tree/gh-pages'),('My HW pages', 'http://asd12357631.github.io/ideal/'))
+         ('Python', 'http://python.org/'),('My Github', 'https://github.com/asd12357631/ideal/tree/gh-pages'),('My HW pages', 'http://asd12357631.github.io/ideal/'),('2015作業主頁', '../../'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
