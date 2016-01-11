52139af5eb21a3f71b136d1422b1be8fb4f53c71
40323212 <40323212@gm.nfu.edu.tw>
2016-01-11 11:06:15 +0800

    40323212

--- user/40323212/pelican/publishconf.py
+++ user/40323212/pelican/publishconf.py
@@ -27,7 +27,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),
-         ('個人倉儲', 'http://40323212.github.io/2055/'),('個人github倉儲', 'https://github.com/40323212/2055/tree/gh-pages/'))
+         ,('個人github倉儲', 'https://github.com/40323212/2055/tree/gh-pages/'),('40323212 個人網頁', 'http://40323212.github.io/2055/'),)
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
