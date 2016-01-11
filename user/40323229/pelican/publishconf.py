d0cf542cbfe3d8ec563998ddc8f364c4dfbb1996
tsrnnash <40323229@gm.nfu.edu.tw>
2016-01-11 10:40:24 +0800

    40323229

--- user/40323229/pelican/publishconf.py
+++ user/40323229/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('40323229 個人網站', 'http://tsrnnash.github.io/40323229-1/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
